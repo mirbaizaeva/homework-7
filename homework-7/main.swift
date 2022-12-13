09dd21e34049546f39972689fea168115678c0ff
Nurjamal <n.mirbaizaeva@gmail.com>
2022-12-13 12:52:14 +0600

    3

--- homework-7/main.swift
+++ homework-7/main.swift
@@ -1,11 +1,52 @@
-//
-//  main.swift
-//  homework-7
-//
-//  Created by Nurjamal Mirbaizaeva on 9/12/22.
-//
-
 import Foundation
+//№1. Составить программу, которая будет добавлять товары в базу данных товаров магазина. Учесть, что у некоторых товаров может не быть некоторых параметров
 
-print("Hello, World!")
+var name: [String] = []
+var producer: [String] = []
+var weight: [Int] = []
+var barcode: [Int] = []
+var price: [Int] = []
+var discount: [Int] = []
+var amount: [Int] = []
+var total: [Int] = []
+var available: [String] = []
+var totalm = 0
+var count = 0
+var title = ""
 
+while title != "стоп" {
+    print("Чтобы добавить товарыб введите ниже 7 пунктов:\n(для завершения введите 'стоп'\nНазвание товара:")
+    let title = readLine()!
+    if title == "стоп" {
+        break
+    }
+    name.append(title)
+    print("Производитель")
+    let producerRedline = readLine()!
+
+    producer.append(producerRedline)
+    print("Вес (кг)")
+    let weighRedline = Int(readLine()!)
+    
+    weight.append(weighRedline ?? 0)
+    print("Штрих-код")
+    let barcodeReadline = Int(readLine()!)
+    
+    barcode.append(barcodeReadline ?? 0)
+    print("Цена (сом)")
+    let priceReadline = Int(readLine()!)
+    
+    price.append(priceReadline ?? 0)
+    print("Скидка (%, если нет - nil)")
+    let discountReadline = Int(readLine()!)
+    
+    discount.append(discountReadline ?? 0)
+    print("Количество (шт)")
+    let amountReadline = Int(readLine()!)
+    
+    amount.append(amountReadline ?? 0)
+    price.append(priceReadline ?? 0)
+    print("Наличие в магазине (да, если нет - nil)")
+    let availableReadline = readLine()!
+    available.append(availableReadline)
+}
